30b3ab6437785f5a895bc774da9fb441bf8af8b7
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-01 02:25:46 +0600

    now hero section all done

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -3,7 +3,7 @@ import { FaMagic } from "react-icons/fa";
 import Button from "../ui/Button";
 import Container from "../shared/Container";
 import { LuShoppingBag } from "react-icons/lu";
-import { BsFire } from "react-icons/bs";
+import { BsFire, BsGift } from "react-icons/bs";
 import Image from "next/image";
 
 const Hero = () => {
@@ -22,7 +22,7 @@ const Hero = () => {
                 </button>
 
             </div> */}
-                <div className="relative grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-3 z-10 justify-center items-center">
+                <div className="relative grid grid-cols-1 lg:grid-cols-2 gap-14 lg:gap-0 z-10 justify-center items-center">
                     <div className="flex flex-col md:text-start space-y-5">
                         <div>
                             <Button text={'NEW SUMMER COLLECTION'} className={'uppercase bg-gray-100 text-[#fd7933] py-1'} leftIcon={<FaMagic />} />
@@ -34,8 +34,17 @@ const Hero = () => {
                             <Button text={'Hot Deals'} leftIcon={<BsFire className="text-[#fd7933]" />} className={'text-slate-800 bg-white'} />
                         </div>
                     </div>
-                    <div className="rounded-2xl bg-linear-to-br from-amber-300 via-orange-400 to-sky-400 p-2">
+                    <div className="relative rounded-2xl bg-linear-to-br from-amber-300 via-orange-400 to-sky-400 p-2">
                             <Image width={500} height={400} className="w-full rounded-2xl " src={'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80'} alt="Beach" priority />
+                            <div className="absolute bottom-6 left-6 lg:-bottom-9 lg:-left-24 bg-white rounded-xl flex gap-2 p-5 text-start justify-center items-center text-[#0C2842] shadow-md">
+                                <div>
+                                    <Image width={40} height={40} src={'https://i.ibb.co.com/gB4p9S1/box.png'} alt="Gift with discount" />
+                                </div>
+                                <div>
+                                    <span className="text-sm font-normal">Free shipping</span>
+                                    <h3 className="font-bold text-base">on orders $50+</h3>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </Container>
