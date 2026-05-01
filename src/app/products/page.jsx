7b07501e612024f79c3ba47c079b8f756542936d
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-01 21:24:13 +0600

    now product page done amazing reuseable product card component

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,8 +1,25 @@
+import Container from "@/components/shared/Container";
+import ProductCard from "@/components/ui/ProductCard";
+import SectionTitle from "@/components/ui/SectionTitle";
+import { getProducts } from "@/lib/actions";
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
+    const products = await getProducts();
+    // console.log(products);
+
     return (
-        <div>
-            Products
+        <div className="py-20">
+            <Container>
+                <div className="text-center">
+                    <SectionTitle paraTitle={'Shop the collection'} HeadingTitle={'All Summer Essentials'} />
+                    <p className="text-[#52657a] text-base font-normal mt-3">Hand-picked products to keep you cool, safe, and stylish.</p>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-5 lg:gap-6 mt-10">
+                    {
+                        products.map(product => <ProductCard key={product.id} product={product} />)
+                    }
+                </div>
+            </Container>
         </div>
     );
 };
