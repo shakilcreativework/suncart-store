62a12847b356385ce4fc5f56ccef119ff7e2f1bb
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-03 11:32:43 +0600

    add loading page for perfectly show data

---
+++ src/app/loading.js
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gray-50">
+      <div className="flex items-center gap-3 text-sm text-[#6B7280] font-medium">
+        <span className="loading loading-spinner text-success w-6 h-6"></span>
+        <span>Loading, please wait...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
