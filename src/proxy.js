0addc547e59709e0b54f5ec2eef5842bbb6fb2b7
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-03 13:10:25 +0600

    now add proxy.js for private route for products any details page, profile, update-profile

--- src/proxy.js
+++ src/proxy.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 import { headers } from "next/headers";
 
-// This function can be marked `async` if using `await` inside
+// This function can be marked `async` if using `await` inside.
 export async function proxy(request) {
   const session = await auth.api.getSession({
     headers: await headers(),
