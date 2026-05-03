147afab2927d2f559a6d9c1d08e4b1ec7df0b13b
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-03 11:49:35 +0600

    all most done now need start private route

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -12,17 +12,17 @@ const NotFound = () => {
                     <div className='text-center'>
 
                         {/* Error Code */}
-                        <h1 className="text-8xl font-extrabold btn-c dm-sans">
+                        <h1 className="text-8xl font-extrabold text-[#0C2842]">
                             404
                         </h1>
 
                         {/* Title */}
-                        <h2 className=" text-xl md:text-3xl font-semibold mt-4 text-[#1F1B16] dm-sans">
+                        <h2 className=" text-xl md:text-3xl font-semibold mt-4 text-[#0C2842]">
                             Oops! Page not found
                         </h2>
 
                         {/* Description */}
-                        <p className="text-[#6F665C] mt-3">
+                        <p className="text-[#6B7280] mt-3">
                             The page you&apos;re looking for doesn’t exist or has been moved.
                         </p>
 
